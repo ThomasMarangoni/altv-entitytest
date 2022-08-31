a74582cb497549263f80fcd7459e5b70b43037f5
Thomas Marangoni <thomas@marangoni.cc>
2022-08-31 23:49:06 +0200

    Init release

---
+++ README.md
@@ -0,0 +1,9 @@
+# alt:V Entity Test
+This is a resource which spawns 7777 vehilces to test the entity system for alt:V. The positions are based on: https://github.com/Hardy535/GT-MP-7777-Random-Vehicle-Spawns 
+
+# Installation
+- Download alt:V and c# module
+- Clone repository to resource folder
+- Add `altv-entitytest` into resource list of server.cfg
+- Run `dotnet build resources/altv-entitytest/src/entitest.csproj` in server directory
+- Start server
---
+++ src/EntityTest.cs
@@ -0,0 +1,67 @@
+﻿using AltV.Net;
+using AltV.Net.Data;
+using AltV.Net.Elements.Entities;
+using AltV.Net.Enums;
+
+
+namespace EntityTest
+{
+    public class EntityTest : Resource
+    {
+        const uint vehicleHash = (uint)VehicleModel.Elegy;
+        static Random rnd = new Random();
+        
+        public static float toRadians(float val)
+        {
+            return (float)((float)val/180) * (float)Math.PI;
+        }
+
+        public static Rgba rndRGB()
+        {
+            return new Rgba((byte)rnd.NextInt64(0, 255), (byte)rnd.NextInt64(0, 255), (byte)rnd.NextInt64(0, 255), 0);
+        }
+
+        public override void OnStart()
+        {
+            Alt.Log("Starting spawning Vehicles");
+
+            int i = 1;
+            int count = SpawnPositions.VehicleList.Count;
+            foreach(var kv in SpawnPositions.VehicleList)
+            {
+                var pos = kv.Key;
+                var rotDeg = kv.Value;
+
+                var rot = new Position(toRadians(rotDeg.Roll), toRadians(rotDeg.Pitch), toRadians(rotDeg.Yaw));
+                var veh = Alt.CreateVehicle(vehicleHash, pos, rot);
+                veh.PrimaryColorRgb = rndRGB();
+                veh.SecondaryColorRgb = rndRGB();
+                veh.NumberplateText = "#" + i;
+
+                i++; 
+            }
+            Alt.Log($"Finished spawning {count} vehicles.");
+
+            Alt.OnPlayerConnect += OnPlayerConnect;
+        }
+
+        public override void OnStop()
+        {
+
+        }
+
+        public void OnPlayerConnect(IPlayer player, string reason)
+        {
+            var spawnPos = new Position(1, 1, 72);
+            player.Model = Alt.Hash("A_M_M_Salton_02");
+            player.Spawn(spawnPos);
+
+            var vehicle = Alt.CreateVehicle(vehicleHash, spawnPos, new Position(0, 0, 0));
+            vehicle.PrimaryColorRgb = new Rgba(255, 0, 0, 0);
+            vehicle.SecondaryColorRgb = new Rgba(255, 0, 0, 0);
+            player.SetIntoVehicle(vehicle, 1);
+        }
+    }
+}
+
+
